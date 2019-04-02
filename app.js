e61c7c9cfab409e45772cfc904a39f9f01d400ea
Jonathan <jonathanman2010@icloud.com>
2019-04-01 18:48:11 -0600

    fixed heroku error

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const path = require('path');
-
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -9,33 +8,31 @@ const passport = require('passport');
 const Yelp = require('yelp-fusion');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/foodie';  // for Heroku connection
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/foodie'; // for Heroku connection
 mongoose.connect(mongoURI);
-
 const index = require('./routes/index');
 const auth = require('./routes/auth');
 const categories = require('./routes/categories')
 const yelp = require('./routes/yelp');
 const app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/materialize', express.static(__dirname + '/node_modules/materialize-css/dist/'));
 app.use('/jquery', express.static(__dirname + '/node_modules/materialize-css/node_modules/jquery/dist/'));
-
 app.use(require('express-session')({
-  secret: process.env.SESSION_SECRET || 'secret',
-  resave: false,
-  saveUninitialized: false
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,29 +40,26 @@ const User = require('./models/user');
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
 app.use('/api/auth', auth);
 app.use('/api/categories', categories);
 app.use('/yelp', yelp)
-
 app.use('*', index);
-
 // catch 404 and forward to error handler
-app.use( (req, res, next) => {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 // error handler
-app.use( (err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
-
 module.exports = app;
